8804c5c27b53ddf69aaa00d9bd3e748d7ef7bb97
Loranxh1 <lx65114@ubt-uni.net>
2024-12-12 14:47:45 +0100

    Lidhje me databaze dhe krijim te routes

--- apps/config.py
+++ apps/config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = 'Miniminterandksi1.'
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'data.db')}"
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'car_sales_data.db')}"
+    # SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'car_sales_data.db')}"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optional but recommended
